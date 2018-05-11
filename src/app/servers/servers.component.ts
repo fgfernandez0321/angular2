5e6f53962217c18fdb62ea2676b3222a928a44ff
Frank Gonzalez Fernandez <fgfernandez0321@gmail.com>
2018-05-11 18:38:30 -0400

    - Adding Warning and Success components.

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+// The selector can act as element, attribute or class
+
 @Component({
   selector: 'app-servers',
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
