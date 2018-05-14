b73a01b384ca277af096848b6fb74e5b61f5d58a
Frank Gonzalez Fernandez <fgfernandez0321@gmail.com>
2018-05-14 12:32:27 -0400

    - Testing binding behaviour.

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -4,4 +4,11 @@ import { Component } from '@angular/core';
   selector: 'app-server',
   templateUrl: './server.component.html'
 })
-export class ServerComponent {}
+export class ServerComponent {
+  serverId = 10; // Or you can define serverId: number = 10; however it is not necessarily because it can be inferred.
+  serverStatus = 'offline';
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+}
