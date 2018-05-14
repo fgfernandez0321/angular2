5b007853df433c27b1b272121d3a64f69b63f0e3
Frank Gonzalez Fernandez <fgfernandez0321@gmail.com>
2018-05-14 17:51:58 -0400

    - Testing ngStyles and ngClass directives

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -16,6 +16,8 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
+  serverName = 'Test Server';
+  serverCreated = false;
 
   constructor() {
     setTimeout( () => {
@@ -27,7 +29,11 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created!';
+    this.serverCreated = true;
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
